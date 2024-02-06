579ce5553852d571902c0b6b59e4b8ac8e7de1f8
Grzegorz Gawrysiak <grzegorz.gawrysiak2@gmail.com>
2024-02-06 22:43:01 +0100

    ProductsList implementation

--- zakupownik/src/components/zaliczenie/ProductsList/ProductsList.jsx
+++ zakupownik/src/components/zaliczenie/ProductsList/ProductsList.jsx
@@ -1,10 +1,39 @@
 import commonColumnsStyles from "../../../common/styles/Columns.module.scss";
-
+import list from "../../../common/consts/produkty";
+import React, { useState } from "react";
 function ProductsList() {
+  const [selectedProducts, setSelectedProducts] = useState([]);
+
+  const handleSelectProduct = (item) => {
+    setSelectedProducts((prevSelectedProducts) => [
+      ...prevSelectedProducts,
+      item,
+    ]);
+  };
+
+  const mappedProductsList = () => {
+    let products = list;
+
+    return (
+      <ul>
+        {products.map((item) => (
+          <li key={item.nazwa} onClick={() => handleSelectProduct(item)}>
+            {item.nazwa}
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
     <div className={commonColumnsStyles.App}>
       <header className={commonColumnsStyles.AppHeader}>
         <p>Products list</p>
+        {mappedProductsList()}
+        <p>
+          Selected Products:{" "}
+          {selectedProducts.map((product) => product.nazwa).join(", ")}
+        </p>
       </header>
     </div>
   );
